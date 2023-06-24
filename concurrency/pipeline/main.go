c5f3b40620038fe1f664c24e349e752e1e6a9253
sonuos1501 <son.uospt@gmail.com>
2023-06-24 17:43:49 +0700

    pipeline

--- concurrency/pipeline/main.go
+++ concurrency/pipeline/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fistChan := firstInput(1, 2, 3, 4, 5, 6)
+	fistChan := firstInput(100, 2, 3, 4, 5, 6)
 	secondChan := secondInput(fistChan)
 
 	for item := range secondChan {
@@ -15,8 +15,8 @@ func firstInput(nums ...int) chan int {
 	result := make(chan int)
 
 	go func() {
-		for i := 0; i < len(nums); i++ {
-			result <- nums[i]
+		for _, item := range nums {
+			result <- item
 		}
 
 		close(result)
